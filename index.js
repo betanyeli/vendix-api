9d4e1009e41b6e1f793ac8564f13686ec8f8f8ef
betanyeli <betangelii@gmail.com>
2023-10-04 00:10:43 -0300

    bugfix: enable cors to avoid warnings in frontend

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors"); // Import the cors middleware
 const app = express();
 const productRoutes = require("./routes/products/index");
 const public = require("./public");
+
+// Enable CORS for all routes.
+app.use(cors());
 
 app.use(express.json());
 
